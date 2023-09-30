b5d1b2978f7e9037d13695c92a1f233f9db52192
mudassar44744 <mudassarmustafai@outlook.com>
2023-09-30 17:23:04 +0500

    containDuplicate

---
+++ Arrays/containDuplicate.js
@@ -0,0 +1,38 @@
+/**
+ *  
+  Given an integer array nums, return true if any value appears at least twice in the array,
+  and return false if every element is distinct.
+  
+  How do you find the duplicate number on a given integer array?
+
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+
+var containsDuplicate = function(nums) {
+    for(let i=0; i<nums.length; i++) {
+        for(let j = i+1; j<nums.length; j++) {
+            if(nums[i] === nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+
+
+
+
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+
+
+
+
