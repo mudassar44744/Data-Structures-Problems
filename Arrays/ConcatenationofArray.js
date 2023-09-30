5592282153c71c92becb814a29a2057e8abbf817
mudassar44744 <mudassarmustafai@outlook.com>
2023-09-30 17:12:48 +0500

    concatenation of two arrays

--- Arrays/ConcatenationofArray.js
+++ Arrays/ConcatenationofArray.js
@@ -2,6 +2,7 @@
 where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 Specifically, ans is the concatenation of two nums arrays.
 Return the array ans.
+
 Example 1:
 
 Input: nums = [1,2,1]
