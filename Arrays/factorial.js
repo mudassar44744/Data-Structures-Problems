3e6d45fd794c23bd3d841efed2e450105f7afd17
mudassar44744 <mudassarmustafai@outlook.com>
2023-10-02 07:37:44 +0500

    factorial

---
+++ Arrays/factorial.js
@@ -0,0 +1,28 @@
+//Write a program to find the factorial of the number.
+
+//Iterative method
+function factorial(number) {
+
+if(number <= 0) return undefined;
+
+let fact = 1;
+
+for (let index = 1; index <= number; index++) {
+    fact *= index; // fact = fact * index;
+}
+return fact;
+
+}
+
+//recursive method
+let number = 4;
+let value = recursiveFactorial(number);
+console.log(value);
+
+function recursiveFactorial(number) {
+    
+    if(number === 0) return 1;
+
+    return   (number *  recursiveFactorial(number - 1));
+
+}
