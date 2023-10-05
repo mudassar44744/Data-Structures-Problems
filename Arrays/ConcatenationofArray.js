4491efcf46a8ede63c08bddce15d9e4d99c1e588
mudassar44744 <mudassarmustafai@outlook.com>
2023-10-05 07:56:49 +0500

    concatenation of two array

--- Arrays/ConcatenationofArray.js
+++ Arrays/ConcatenationofArray.js
@@ -22,7 +22,7 @@ var getConcatenation = function(nums) {
     const ans = [];
     for(let i=0; i<nums.length; i++) {
 
-        ans[i] = nums[i];
+        ans[i] = nums[i];   
         ans[i + nums.length] = nums[i];
     }
     return result;
