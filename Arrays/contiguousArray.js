f3ddbe3f6603aabfed816b96820a319bc425836c
mudassar44744 <mudassarmustafai@outlook.com>
2023-10-07 11:51:52 +0500

    contiguous subarray

---
+++ Arrays/contiguousArray.js
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/contiguous-array/
+// https://leetcode.com/problems/contiguous-array/solutions/4074726/simple-javascript-solution/
+//https://www.youtube.com/watch?v=6PAj5F9O4gU&ab_channel=CodewithAlisha
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function(nums) {
+    let map = {0:-1};
+    let count = 0;
+    let max = 0;
+    for (let i=0;i<nums.length;i++) {
+        if (nums[i] == 0) count--;
+        else count++;
+
+        if (map[count]!=null) 
+        {
+            max = Math.max(max, i - map[count]);
+        }
+        else { 
+            map[count] = i
+        }
+    }
+    return max;
+};
+
+
+console.log(
+    findMaxLength([0,1,0,1])
+);
+
+/**
+ 0 - -1 = -1 ; 
+ values : 0 1 0 1 
+ index : 0 1 2 3
+ ans : -1 0 -1 0
+ map : { 0 : -1, -1 : 0 }
+
+
+
+ */
